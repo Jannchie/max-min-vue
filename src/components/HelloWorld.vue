4cba264b96ea58833e62d17affe46d4c31ea6a20
Jianqi Pan <jannchie@gmail.com>
2024-09-09 23:13:17 +0900

    chore(dependencies): upgrade to latest

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,9 @@ const hello = useHelloWorld()
 </script>
 
 <template>
-  <div class="bg-[#090909] text-[#E9994C] h-100vh w-100vw flex items-center justify-center">
+  <div class="h-100vh w-100vw flex items-center justify-center bg-[#090909] text-[#E9994C]">
     <div class="relative">
-      <div class="text-[#cc4c19] font-bold absolute top-0 blur-20 text-nowrap pointer-event-none select-none">
+      <div class="pointer-events-none absolute top-0 select-none text-nowrap text-[#cc4c19] font-bold blur-20">
         {{ hello }}
       </div>
       <div>
