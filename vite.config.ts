5108f6f2e00e22714fdac9611943c50c4c9386b9
Jianqi Pan <jannchie@gmail.com>
2024-10-24 21:50:38 +0900

    feat(deps): add @types/node to devDependencies && update pnpm-lock.yaml for compatibility && configure alias in tsconfig and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,4 +30,9 @@ export default defineConfig({
       dts: './src/auto-import.d.ts',
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
