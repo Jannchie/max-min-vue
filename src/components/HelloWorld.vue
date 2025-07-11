70a5001d8b7471b436e644075cf1e08dd48b98f2
Jannchie <jannchie@gmail.com>
2025-07-11 21:42:09 +0900

    style(helloworld): reorder css utility classes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,9 @@ const hello = useHelloWorld()
 </script>
 
 <template>
-  <div class="h-100vh w-100vw flex items-center justify-center bg-[#090909] text-[#E9994C]">
+  <div class="text-[#E9994C] bg-[#090909] flex h-100vh w-100vw items-center justify-center">
     <div class="relative">
-      <div class="pointer-events-none absolute top-0 select-none text-nowrap text-[#cc4c19] font-bold blur-20">
+      <div class="text-[#cc4c19] font-bold pointer-events-none select-none text-nowrap top-0 absolute blur-20">
         {{ hello }}
       </div>
       <div>
